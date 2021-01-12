c5909599ec2c11b46840111936ba1cee601e3796
Linda Huynh <Lindahuynh06@gmail.com>
2021-01-12 15:03:44 -0600

    JS update

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,30 @@
-import "./index.css";
-
 const openModalButton = document.querySelector('.profile__edit-button');
-const closeModalButton = document.querySelector('.modal__btn-close');
+const closeModalButton = document.querySelector('.modal__button');
 const modal = document.querySelector('.modal');
 const form = document.querySelector('.form');
 const nameInput = document.querySelector('.form__input_type_name');
 const descriInput = document.querySelector('.form__input_type_description');
+const profileName = document.querySelector('.profile__name');
+const profileDescri = document.querySelector('.profile__description');
 
 function toggleModalWindow() {
     modal.classList.toggle('modal_open')
 }
 
-openModalButton.addEventListener('click', function () {
+openModalButton.addEventListener('click' , function() {
     toggleModalWindow()
+    // form.reset();
 
     nameInput.value = profileName.textContent;
-    descriInput.value = profileDescri.textContent;
+    descriInput.value = profileDescri.textcontent;
 })
 
-closeModalButton.addEventListener('click', toggleModalWindow)
 
-form.addEventListener('submit', function () {
-    profileName.textContent = nameInput.value;
-    profileDescri.textContent = descriInput.value;
+closeModalButton.addEventListener('click' , toggleModalWindow)
+
+form.addEventListener('click' , function(){
+    profileName.textcontent = nameInput.value;
+    profileDescri.textcontent = descriInput.value;
 
     toggleModalWindow()
 })
