91b430b83a185cf8440cfc2a3620cad9187fa85a
Linda Huynh <Lindahuynh06@gmail.com>
2021-01-12 13:57:09 -0600

    Updated Modal

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import "./index.css";
 
 const openModalButton = document.querySelector('.profile__edit-button');
 const closeModalButton = document.querySelector('.modal__btn-close');
-const addButton = document.querySelector('.profile__add-button');
 const modal = document.querySelector('.modal');
 const form = document.querySelector('.form');
 const nameInput = document.querySelector('.form__input_type_name');
@@ -21,24 +20,9 @@ openModalButton.addEventListener('click', function () {
 
 closeModalButton.addEventListener('click', toggleModalWindow)
 
-form.addEventListener('submit', function(){
+form.addEventListener('submit', function () {
     profileName.textContent = nameInput.value;
     profileDescri.textContent = descriInput.value;
 
     toggleModalWindow()
 })
-
-function qs(selector) {
-    return document.querySelector(selector);
-  }
-  
-  function formSubmitHandler(evt) {
-    evt.preventDefault();
-    qs('.profile__name').textContent = qs('.form__input_type_name').value;
-    qs('.profile__description').textContent = qs('.form__input_type_description').value;
-    closeEditForm();
-  }
-  
-  function closeEditForm() {}
-  
-  document.getElementById('submit').addEventListener('click', formSubmitHandler);
