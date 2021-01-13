b4e98cd5204761ed472ead0da5887ce511e34e18
Linda Huynh <Lindahuynh06@gmail.com>
2021-01-12 21:45:16 -0600

    Updated Form

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,32 @@
-let editButton = document.querySelector('.profile__edit-button');
-let addButton = document.querySelector('.profile__add-button');
-let profileName = document.querySelector('.profile__name');
-let profileDescri = document.querySelector('.profile__description');
-let form = document.querySelector('.form');
-let btnClose = document.querySelector('.form__btn-close');
-let nameInput = document.querySelector('.form__input_type_name');
-let descriInput = document.querySelector('.form__input_type_description');
+let editButton = document.querySelector(".profile__edit-button");
+let addButton = document.querySelector(".profile__add-button");
+let profile = document.querySelector(".profile")
+let profileName = document.querySelector(".profile__name");
+let profileDescri = document.querySelector(".profile__description");
+let form = document.querySelector(".form");
+let formSave = document.querySelector(".form__save-button")
+let btnClose = document.querySelector(".form__btn-close");
+let formName = document.querySelector(".form__input_type_name");
+let formDescri = document.querySelector(".form__input_type_description");
 
-function openForm() {
-    let open = document.querySelector('.profile__edit-button');
-    opened.classList.add('form_opened');
+function formOpen() {
+    opened.classList.add("form_opened");
 }
 
-function closeForm() {
-    let closed = document.querySelector('.form__btn-close');
-    closed.classList.remove('form_opened');
+editButton.addEventListener("click", formOpen);
+
+function formClose() {
+    closed.classList.remove("form_opened");
 }
+
+btnClose.addEventListener("click", formClose);
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
-    nameInput.value = profileName.textContent;
+    nameInput.value = profileName.textcontent;
     descriInput.value = profileDescri.textcontent;
-    profileName.textcontent = nameInput.value;
-    profileDescri.textcontent = descriInput.value;
+    profileName.textcontent = `${formName.value}`;
+    profileDescri.textcontent = `${formDescri.value}`;
 }
 
-// Connect the handler to the form:
-// it will watch the submit event
-editButton.addEventListener('click', openForm);
-btnClose.addEventListener('click', closeForm);
-form.addEventListener('submit', handleFormSubmit);
+formSave.addEventListener("submit", handleFormSubmit);
