02d83848d0cae7d2c5e263265aab49899e350497
Linda Huynh <Lindahuynh06@gmail.com>
2021-01-13 11:57:34 -0600

    Updated Form CSS

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 let editButton = document.querySelector(".profile__edit-button");
-let addButton = document.querySelector(".profile__add-button");
 let profile = document.querySelector(".profile")
 let profileName = document.querySelector(".profile__name");
 let profileDescri = document.querySelector(".profile__description");
@@ -9,24 +8,44 @@ let btnClose = document.querySelector(".form__btn-close");
 let formName = document.querySelector(".form__input_type_name");
 let formDescri = document.querySelector(".form__input_type_description");
 
-function formOpen() {
-    opened.classList.add("popup_open");
+function toggleModalWindow() {
+    form.classList.toggle('popup_open')
 }
 
-editButton.addEventListener("click", formOpen);
+editButton.addEventListener('click' , functional() {
+    toggleModalWindow()
 
-function formClose() {
-    closed.classList.remove("popup_open");
-}
+    formName.value = profileName.textContent;
+    formDescri = profileDescri.textContent;
 
-btnClose.addEventListener("click", formClose);
+})
+btnClose.addEventListener('click' , toggleModalWindow)
 
-function handleFormSubmit(evt) {
-    evt.preventDefault();
-    nameInput.value = profileName.textcontent;
-    descriInput.value = profileDescri.textcontent;
-    profileName.textcontent = `${formName.value}`;
-    profileDescri.textcontent = `${formDescri.value}`;
-}
+form.addEventListener('submit' , function() {
+    profileName.textcontent = formName.value;
+    profileDescri.textContent = formDescri.value;
 
-formSave.addEventListener("submit", handleFormSubmit);
+    toggleModalWindow()
+})
+
+// function formOpen() {
+//     opened.classList.add("popup_open");
+// }
+
+// editButton.addEventListener("click", formOpen);
+
+// function formClose() {
+//     closed.classList.remove("popup_open");
+// }
+
+// btnClose.addEventListener("click", formClose);
+
+// function handleFormSubmit(evt) {
+//     evt.preventDefault();
+//     nameInput.value = profileName.textcontent;
+//     descriInput.value = profileDescri.textcontent;
+//     profileName.textcontent = `${formName.value}`;
+//     profileDescri.textcontent = `${formDescri.value}`;
+// }
+
+// formSave.addEventListener("submit", handleFormSubmit);
