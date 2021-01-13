67405315c0c75e8c537fa29732e70dc96953062d
Linda Huynh <Lindahuynh06@gmail.com>
2021-01-13 12:05:07 -0600

    Updated  Modal

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,22 @@ let editButton = document.querySelector(".profile__edit-button");
 let profile = document.querySelector(".profile")
 let profileName = document.querySelector(".profile__name");
 let profileDescri = document.querySelector(".profile__description");
+let modal = document.querySelector(".modal");
 let form = document.querySelector(".form");
 let formSave = document.querySelector(".form__save-button")
-let btnClose = document.querySelector(".form__btn-close");
+let btnClose = document.querySelector(".modal__btn-close");
 let formName = document.querySelector(".form__input_type_name");
 let formDescri = document.querySelector(".form__input_type_description");
 
 function toggleModalWindow() {
-    form.classList.toggle('popup_open')
+    modal.classList.toggle('modal_open')
 }
 
 editButton.addEventListener('click' , functional() {
     toggleModalWindow()
 
     formName.value = profileName.textContent;
-    formDescri = profileDescri.textContent;
+    formDescri.value = profileDescri.textContent;
 
 })
 btnClose.addEventListener('click' , toggleModalWindow)
